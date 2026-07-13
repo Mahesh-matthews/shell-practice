fc547a878fc03790c2e72a134b35bd8dfd01a0af
Mahesh Kasapu <mahesh.kasapu@meridianitinc.com>
2026-07-13 09:58:36 +0530

    variable added

--- sample.sh
+++ sample.sh
@@ -1,3 +1,11 @@
 #!/bin/bash
 echo "helloworld"
 echo "i have added a line"
+
+person1=Evan
+perrson2=DadMom
+
+echo "$person1:: Hello $person2, how are you?"
+echo "$person2:: Hello $person1, iam Fine, how are you?"
+echo "$person1:: Hello $person2, I am fine too?"
+echo "$person2:: Hello $person1, Glad to know that?"
