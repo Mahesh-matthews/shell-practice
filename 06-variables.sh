daa92a89f15e9d6db220662fbdc2870614c7576b
Mahesh Kasapu <mahesh.kasapu@meridianitinc.com>
2026-07-13 10:51:37 +0530

    04variables

--- 06-variables.sh
+++ 06-variables.sh
@@ -1,9 +1,14 @@
-START_TIME=$(date +$s)
+#!/bin/bash
 
-echo "Script executed at : $START_TIME"
+START_TIME=$(date +%s)
+
+echo "Script started at: $START_TIME"
 
 sleep 10
 
-END_TIME=$(date +$s)
-TOTAL_TIME=$(($END_TIME-$START_TIME))
-echo "Script Exected time is: $TOTAL_TIME" 
+END_TIME=$(date +%s)
+
+TOTAL_TIME=$((END_TIME - START_TIME))
+
+echo "Script ended at: $END_TIME"
+echo "Script execution time is: $TOTAL_TIME seconds"
