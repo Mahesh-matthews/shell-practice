14ef0a512cfbca979c78757bbfe64a47b4b84220
Mahesh Kasapu <mahesh.kasapu@meridianitinc.com>
2026-07-13 14:01:47 +0530

    04variables

--- datatypes.sh
+++ datatypes.sh
@@ -8,7 +8,7 @@ echo "Sume is : $sum"
 
 #Array
 FRUITS=("Apple" "Banana" "Pomo")
-echo "Fruites are : $FRUITS[@]"
-echo "1st fruit is : $FRUITS[0]"
-echo "2nd fruit is : $FRUITS[1]"
-echo "3rd fruit is : $FRUITS[2]"
+echo "Fruites are : ${FRUITS[@]}"
+echo "1st fruit is : ${FRUITS[0]}"
+echo "2nd fruit is : ${FRUITS[1]}"
+echo "3rd fruit is : ${FRUITS[2]}"
