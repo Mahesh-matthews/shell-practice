4e141ec578e1f6e93d7ebba7f72f6ad9796957b2
Mahesh Kasapu <mahesh.kasapu@meridianitinc.com>
2026-07-13 13:58:58 +0530

    04variables

---
+++ datatypes.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+NUM1=100
+NUM2=200
+sum=$((NUM1+NUM2))
+
+echo "Sume is : $sum"
+
+#Array
+FRUITS=("Apple" "Banana" "Pomo")
+echo "Fruites are : $FRUITS[@]"
+echo "1st fruit is : $FRUITS[0]"
+echo "2nd fruit is : $FRUITS[1]"
+echo "3rd fruit is : $FRUITS[2]"
