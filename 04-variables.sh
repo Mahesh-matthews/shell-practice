bf585dee49e7767d9fb8a76d653472ceffe2409b
Mahesh Kasapu <mahesh.kasapu@meridianitinc.com>
2026-07-13 10:40:59 +0530

    04variables

--- 04-variables.sh
+++ 04-variables.sh
@@ -6,4 +6,4 @@ echo "User name is $USER_NAME"
 echo "Please enter your Password"
 read -s PASSWORD
 #dont print password. its only for testing purpose
-echo "User name is $PASSWORD"
+echo "User Password is $PASSWORD"
---
+++ 05-variables.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+#I want a commnand to be executed and take the output into variable
+
+
+TIMESTAMP=$(date)
+echo "This Script Executed at : $TIMESTAMP "
