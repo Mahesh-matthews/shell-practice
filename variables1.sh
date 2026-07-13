b1d6b02ffd29334e5f4966b27941980d7e1d39f5
Mahesh Kasapu <mahesh.kasapu@meridianitinc.com>
2026-07-13 10:20:35 +0530

    variables

---
+++ variables1.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+person1=$1
+person2=$2
+echo "$person1:: Hello $person2, how are you?"
+echo "$person2:: Hello $person1, iam Fine, how are you?"
+echo "$person1:: Hello $person2, I am fine too?"
+echo "$persosn2:: Hello $person1, Glad to know that?"
