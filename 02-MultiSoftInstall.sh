cbefa8b78fdc2bd9b8776d893e22f74cffe131fd
Mahesh Kasapu <mahesh.kasapu@meridianitinc.com>
2026-07-13 16:40:56 +0530

    04variables

---
+++ 02-MultiSoftInstall.sh
@@ -0,0 +1,26 @@
+#!/bin/bash
+
+USERID=$(id -u)
+if [ "$USERID" -ne 0 ]; then
+    echo "Please login to root to run this script"
+    exit 1
+fi
+echo "Installing nginx"
+dnf install nginx -y
+
+if [ $? -ne 0 ]; then
+    echo "Installing nginx.... Failure"
+    exit 1
+else 
+    echo "Installing nginx.... Success"
+fi
+
+dnf install mysql -y
+
+if [ $? -ne 0]; then
+    echo "Installing mySQL... failure"
+    exit 1
+else 
+    echo "Installing mySQL... Success"
+    exit 1
+fi
