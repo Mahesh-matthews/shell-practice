a4728766269e3f19151f06a43dfd41526f69c60a
Mahesh Kasapu <mahesh.kasapu@meridianitinc.com>
2026-07-13 15:58:56 +0530

    04variables

--- Installation.sh
+++ Installation.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 
 USERID=$(id -u)
-if ($USERID != 0); then
-    echo "Lgoin in to Root"
+if [ "$USERID" -ne 0 ]; then
+    echo "Please login to root to run this script"
 fi
 echo "Installing nginx"
 dnf install nginx -y
