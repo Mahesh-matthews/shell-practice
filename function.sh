105e67d3c315e17dff1adb6503ee966688200eb8
Mahesh Kasapu <mahesh.kasapu@meridianitinc.com>
2026-07-13 17:07:50 +0530

    04variables

---
+++ function.sh
@@ -0,0 +1,26 @@
+#!/bin/bash
+
+USERID=$(id -u)
+if [ "$USERID" -ne 0 ]; then
+    echo "Please login to root to run this script"
+    exit 1
+fi
+
+VALIDATE(){
+if [ $1 -ne 0 ]; then
+    echo "$2....... Failure"
+    exit 1
+else 
+    echo "$2....... Success"
+fi
+
+}
+
+dnf install nginx -y
+VALIDATE $? "Installing nginx"
+
+dnf install mysql8.4-server -y
+VALIDATE $? "Installing mysql"
+
+dnf install nodejs24 -y
+VALIDATE $? "Installing nodejs24"
