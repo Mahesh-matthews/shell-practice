9320a4f38b30005542cadaa516b80383687ee2fb
Mahesh Kasapu <mahesh.kasapu@meridianitinc.com>
2026-07-13 10:36:41 +0530

    04variables

---
+++ 04-variables.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+echo "please enter your username:"
+read USER_NAME
+echo "User name is $USER_NAME"
+echo "Please enter your Password"
+read -s PASSWORD
+#dont print password. its only for testing purpose
+echo "User name is $PASSWORD"
